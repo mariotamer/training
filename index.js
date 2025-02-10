ee58a1af4172fcabe82e2a9b0c723759541f10e8
mariotamer <160607561+mariotamer@users.noreply.github.com>
2025-02-11 00:10:27 +0200

    2nd commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-function test()
-{
-    alert("Ayre bl dares");
+function test() {
+  alert("Ayre bl dares");
 }
+
+function RenderStudents() {
+  let ddl_students = document.getElementById('ddl_students');
+  fetch("https://s6tr-api.onrender.com/Students")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(student => {
+            let option = document.createElement('option');
+            option.innerHTML = student.name;
+            ddl_students.appendChild(option);
+        });
+    });
+}
+
+RenderStudents();
